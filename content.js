28d2aaaf8028e8cff2e3a7d8d3d6399c9f22990e
Kartikey <negikartikey22@gmail.com>
2026-08-24 13:24:51 +0530

    added the note button inn youtube video itself

--- content.js
+++ content.js
@@ -17,3 +17,56 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
     }
 
 });
+
+const addCustomButton = () => {
+    let controls = document.querySelector('.ytp-right-controls');
+
+    if(!controls || document.getElementById('vidnote-btn')) return;
+    
+    let btn = document.createElement('button');
+    btn.id = 'vidnote-btn';
+    btn.className = 'ytp-button';
+    btn.style.width = 'auto';
+    btn.style.padding = '0 10px';
+    btn.style.color = 'white';
+    btn.style.fontSize = '13px';
+    btn.style.fontWeight = 'bold';
+
+    btn.style.display = 'flex';
+    btn.style.alignItems = 'center';
+    btn.style.justifyContent = 'center';
+
+    btn.innerText = '📝 Note'
+
+    btn.addEventListener('click', () =>{
+        let vid = document.querySelector('video');
+        if (!vid) return;
+
+        vid.pause();
+
+        let time = Math.floor(vid.currentTime);
+
+        let txt = prompt("What do you want to note?");
+
+        if (txt && txt.trim() !== ''){
+            let url = new URL(window.location.href);
+            let vidId = url.searchParams.get("v");
+
+            chrome.storage.local.get([vidId], (data)=>{
+                let notes = data[vidId] || [];
+                notes.push({ time: time, text: txt, id: Date.now() });
+                notes.sort((a,b) => a.time - b.time);
+
+                chrome.storage.local.set({ [vidId]: notes}, ()=>{
+                    vid.play();
+                });
+            });
+        } else {
+            vid.play();
+        }
+    });
+
+    controls.insertBefore(btn, controls.firstChild);
+};
+
+setInterval(addCustomButton, 1000);
